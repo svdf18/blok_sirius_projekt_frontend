b6959c5aa5896e56c40903e62d467a3d7b669133
Rolf <rarogbennu@gmail.com>
2023-11-23 08:27:12 +0100

    Admin

---
+++ src/components/AdminSection/AdminElements.jsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const AdminContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    position: relative;
+    background: #FFEBA4;
+`
+
+export const AdminH1Container = styled.div`
+    position: fixed;
+    top: 0;
+    width: 100%;
+    background: #FFEBA4;
+    text-align: center;
+    padding: 20px;
+    z-index: 100;
+`
+
+export const AdminH1 = styled.h1`
+    font-size: 48px;
+`
