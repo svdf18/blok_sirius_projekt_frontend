3fa34d85ade4433c688d4c6e66ed604f9b7bb3ef
Rolf <rarogbennu@gmail.com>
2023-11-23 22:27:00 +0100

    to try the form

--- src/components/AdminSection/AdminComponent.jsx
+++ src/components/AdminSection/AdminComponent.jsx
@@ -1,7 +1,7 @@
 import { AdminContainer, AdminH1Container, AdminH1, } from './AdminElements.jsx';
 import GlobalStyle from '../../styles/globalStyles.jsx';
 import Card from '../../utils/CardUtil/CardComponent.jsx';
-
+import Form from '../Form/FormComponent.jsx';
 
 const AdminSection = () => {
 
@@ -13,6 +13,7 @@ const AdminSection = () => {
         <AdminH1Container>
         <AdminH1>Sirius</AdminH1>
         </AdminH1Container>
+        <Form/>
         <Card color="white" buttonText="Manage People Directory" buttonRoute="/people-directory" />
         <Card color="green" buttonText="Manage Knowledge Hub" buttonRoute="/knowledge-hub" />
       </AdminContainer>
