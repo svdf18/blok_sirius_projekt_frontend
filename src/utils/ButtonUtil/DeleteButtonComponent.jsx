a8160415a405d2b5e9d3a7cf1803de2c7da1fa01
Rolf <rarogbennu@gmail.com>
2023-11-29 11:48:09 +0100

    Buttons

--- src/utils/ButtonUtil/DeleteButtonComponent.jsx
+++ src/utils/ButtonUtil/DeleteButtonComponent.jsx
@@ -1,7 +1,14 @@
+import PropTypes from 'prop-types';
+
 export const DeleteButtonComponent = ({ onDelete, itemId, ...restProps }) => {
   const handleClick = () => {
     onDelete(itemId);
   };
 
   return <button onClick={handleClick} {...restProps}>Delete</button>;
+};
+
+DeleteButtonComponent.propTypes = {
+  onDelete: PropTypes.func.isRequired,
+  itemId: PropTypes.number.isRequired,
 };
--- src/utils/ButtonUtil/UpdateButtonComponent.jsx
+++ src/utils/ButtonUtil/UpdateButtonComponent.jsx
@@ -1,7 +1,14 @@
+import PropTypes from 'prop-types';
+
 export const UpdateButtonComponent = ({ onUpdate, itemId, ...restProps }) => {
   const handleClick = () => {
     onUpdate(itemId, restProps);
   };
 
   return <button onClick={handleClick}>Update</button>;
+};
+
+UpdateButtonComponent.propTypes = {
+  onUpdate: PropTypes.func.isRequired,
+  itemId: PropTypes.number.isRequired,
 };
