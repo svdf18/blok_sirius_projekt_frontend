fa1d6c1563d3d7201360121521f1a62943a1b22e
svdf18 <simonvandeurs@hotmail.com>
2023-12-13 16:26:05 +0100

    ddd

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeIndex from './pages/HomeIndex.jsx';
 import PeopleDirectoryIndex from './pages/PeopleDirectoryIndex.jsx';
 import LogInIndex from './pages/LogInIndex.jsx';
