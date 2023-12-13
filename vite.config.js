3a9ca1f6b5649db5c51940649c063a9a2ee6f1d9
svdf18 <simonvandeurs@hotmail.com>
2023-12-13 16:19:56 +0100

    deploy vercel test

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/blok_sirius_projekt_frontend/',
+  // base: '/blok_sirius_projekt_frontend/',
 })
